919ce79142723d86c68b05daa7eeaaca878d04fd
NV <nvardell@gmail.com>
2019-07-01 13:05:31 -0400

    Initial commit

---
+++ src/main/java/com/stated/royally/RoyallyStatedApplication.java
@@ -0,0 +1,13 @@
+package com.stated.royally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyallyStatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyallyStatedApplication.class, args);
+	}
+
+}
