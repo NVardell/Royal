321c5b25619764bc98cf562162cd13e15670054d
NV <nvardell@gmail.com>
2019-09-04 13:42:25 -0400

    Misc. unit tests

---
+++ src/test/java/com/stated/royally/other/URLEncodeTest.java
@@ -0,0 +1,26 @@
+package com.stated.royally.other;
+
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Encoding Unit Tests
+ *
+ * @author Nate Vardell
+ * @since 8/18/2019
+ */
+public class URLEncodeTest {
+
+    @Test
+    public void testHashTagEncoding() throws UnsupportedEncodingException {
+        assertThat(URLEncoder.encode("#PPLYL09L", "UTF-8"), is("%23PPLYL09L"));
+        assertThat(StandardCharsets.UTF_8.name(), is("UTF-8"));
+    }
+
+}
