f85586d391e07fcfb9e6977ff700530f5e2a6e62
NV <nvardell@gmail.com>
2020-02-01 21:37:31 -0500

    Created War Resource class

---
+++ royal-rest/src/main/java/com/stated/royally/war/WarResource.java
@@ -0,0 +1,27 @@
+package com.stated.royally.war;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * War Resource
+ * https://api.clashroyale.com/v1/clans/{{clanTag}}/warlog
+ *
+ * @author Nate Vardell
+ * @since 2/1/2020
+ */
+@RestController
+@RequestMapping("wars")
+public class WarResource {
+
+    // Unirest offers a lightweight JSON response type when you don’t need a full Object Mapper.
+//    String result = Unirest.get("http://some.json.com")
+//            .asJson()
+//            .getBody()
+//            .getObject()
+//            .getJSONObject("thing")
+//            .getJSONArray("foo")
+//            .get(0)
+
+
+}
