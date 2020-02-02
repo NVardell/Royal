50b6fc82a3d8070f34f3e00e19124ca782e31b68
NV <nvardell@gmail.com>
2020-02-01 21:37:14 -0500

    Created War model class

---
+++ royal-rest/src/main/java/com/stated/royally/war/War.java
@@ -0,0 +1,16 @@
+package com.stated.royally.war;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+/**
+ * War Details
+ *
+ * @author Nate Vardell
+ * @since 2/1/2020
+ */
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class War {
+
+}
