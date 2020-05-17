1d5ccfe4b6b824a55c4a26b4524cbedf43471eb4
NV <nvardell@gmail.com>
2020-05-17 14:22:33 -0400

    Removed autowires & added additional config values

--- royal-rest/src/main/java/com/stated/royally/config/UniConfig.java
+++ royal-rest/src/main/java/com/stated/royally/config/UniConfig.java
@@ -1,42 +1,43 @@
 package com.stated.royally.config;
 
 import kong.unirest.Unirest;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 /**
- * TODO - Add Class Definition
+ * UniRest Config
  *
  * @author Nate Vardell
  * @since 7/1/2019
  */
-@Slf4j
+@Log4j2
 @Component
 public class UniConfig {
 
-    @Autowired Environment env;
+    @Value("${official.resource.base}") String baseUrl;
+    @Value("${clash.royale.dev.api.token}") String jwt;
 
     @PostConstruct
     public void init() {
         log.info("Configuring Uni.");
 
-        String token = "Bearer " + env.getRequiredProperty("clash.royale.dev.api.token");
-        log.info("Bearer Token is = {}", token);
-
         Unirest.config()
-                .setObjectMapper(new CustomObjectMapper())
-                .setDefaultHeader("Authorization", token)
-                // For metrics
+                // Setup Metrics
                 .instrumentWith(requestSummary -> {
-                    long startNanos = System.nanoTime();
-                    return (responseSummary,exception) -> log.info("path: {} status: {} time: {}",
-                            requestSummary.getRawPath(),
-                            responseSummary.getStatus(),
-                            System.nanoTime() - startNanos);
-                });
+                    long startNano = System.nanoTime();
+                    return (responseSummary, exception) ->
+                            log.info("path: {} status: {} time: {}",
+                                    requestSummary.getRawPath(),
+                                    responseSummary.getStatus(),
+                                    System.nanoTime() - startNano);
+                })
+                // Set Custom Object Mapper
+                .setObjectMapper(new CustomObjectMapper())
+                // Set Default Values
+                .setDefaultHeader("Authorization", "Bearer " + jwt)
+                .defaultBaseUrl(baseUrl);
     }
 }
