133c122a4145a7e351f6d4218d52879062ebf805
NV <nvardell@gmail.com>
2020-05-17 13:09:50 -0400

    Added NTS

--- royal-rest/src/main/java/com/stated/royally/RoyalApp.java
+++ royal-rest/src/main/java/com/stated/royally/RoyalApp.java
@@ -3,6 +3,11 @@ package com.stated.royally;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * NTS: Look at RoyaleAPI Impl for Ideas.
+ * https://github.com/mlieshoff/jcrapi/blob/master/README.md
+ */
 @SpringBootApplication
 public class RoyalApp {
 
