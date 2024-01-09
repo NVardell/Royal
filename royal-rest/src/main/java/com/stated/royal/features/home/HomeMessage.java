da1190754bec341493d5029dd295499d05f5de9c
NV <nvardell@gmail.com>
2024-01-09 17:30:40 -0500

    home test message pojo

--- royal-rest/src/main/java/com/stated/royal/features/home/HomeMessage.java
+++ royal-rest/src/main/java/com/stated/royal/features/home/HomeMessage.java
@@ -1,7 +1,15 @@
-package com.stated.royal.features.home;/**
+package com.stated.royal.features.home;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
  * TODO - Add Class Definition
  *
  * @author NV
  * @since 8/23/2023
- */public class HomeMessage {
+ */
+@Data @Builder public class HomeMessage {
+    private int status;
+    private String message;
 }
